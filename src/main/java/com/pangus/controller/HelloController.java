b38fe73a9c457ecdc2f83b80cfc42ec95b1749b1
laizt <1070718017@qq.com>
2023-01-08 11:33:27 +0800

    第二次改动

--- src/main/java/com/pangus/controller/HelloController.java
+++ src/main/java/com/pangus/controller/HelloController.java
@@ -10,6 +10,7 @@ public class HelloController {
     @RequestMapping("/world")
     public String sayHi(){
         System.out.println("abc");
+        System.out.println("efg");
         return "helloworld";
     }
 }
