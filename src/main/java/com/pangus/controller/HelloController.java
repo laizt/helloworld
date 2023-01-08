f8c43d1d0689254c9558a40259b84edf94a6ab2a
laizt <1070718017@qq.com>
2023-01-08 11:25:22 +0800

    初始化

---
+++ src/main/java/com/pangus/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.pangus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/world")
+    public String sayHi(){
+        return "helloworld";
+    }
+}
